8d3f0b144d1c931abf7423d674344350eebc4d6e
shahzad11khan <zarakali401@gmail.com>
2025-07-09 17:10:38 +0500

    add api get all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,3 +66,13 @@ exports.updateUser = async (req, res) => {
     res.status(500).json({ message: "Failed to update user", error });
   }
 };
+
+// Get all user
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch users", error });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,7 @@ const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/register", userController.registerUser);
 router.post("/login", userController.loginUser);
+router.get("/getAllUsers", authMiddleware, userController.getAllUsers);
 router.get("/me", authMiddleware, userController.getMe);
 router.put("/update", authMiddleware, userController.updateUser);
 
