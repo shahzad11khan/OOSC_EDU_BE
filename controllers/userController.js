3ec4184263491e31d7ae6851b0b1e124b6ebe1a1
shahzad11khan <zarakali401@gmail.com>
2025-07-09 17:51:00 +0500

    delete api integration

--- controllers/userController.js
+++ controllers/userController.js
@@ -76,3 +76,14 @@ exports.getAllUsers = async (req, res) => {
     res.status(500).json({ message: "Failed to fetch users", error });
   }
 };
+
+// Delete user
+exports.deleteUser= async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) return res.status(404).json({ message: "User not found" });
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete user", error });
+  }
+};
