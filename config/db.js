cdce38a460161d0254d950ce7c472c9b83d17f99
shahzad11khan <zarakali401@gmail.com>
2025-08-07 14:15:20 +0500

    change the variable name

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI+process.env.DB_NAME);
+    await mongoose.connect(process.env.MONGO_URI+process.env.DATABASE_NAME);
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection failed", err);
