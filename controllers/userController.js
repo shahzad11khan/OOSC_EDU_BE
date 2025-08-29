d0ba21d64a72af01b3b816b27f4db0f0e5e1e02b
Atif Afridi <atifafridi378@gmail.com>
2025-08-29 11:18:31 +0500

    password hidden

--- controllers/userController.js
+++ controllers/userController.js
@@ -91,7 +91,7 @@ exports.updateUser = async (req, res) => {
 // Get all user
 exports.getAllUsers = async (req, res) => {
   try {
-    const users = await User.find().select("-password");
+    const users = await User.find().select("-password -confirmPassword");
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: "Failed to fetch users", error });
